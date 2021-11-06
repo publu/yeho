943c60da7f7351fa39f0132af8418a0cbdad0f69
Pablo <Pjs130130@utdallas.edu>
2021-11-06 13:52:41 +0000

    kraken

--- testing/data.js
+++ testing/data.js
@@ -7,12 +7,17 @@ module.exports = {
     binance: {
       apiKey: "9gcYJjQtcxf4Xy6xFL3se2WZYbMXRPAKZWUA8a8q6lxFwkKUV9QbVn6JMRuafrwH",
       secret: "Gf9dADq42RvoIAmtRWuvHDjRK79S4lW8TZr0peI1zOlALV5TjrHj3eNGNTd2FtGm"
+    },
+    kraken: {
+      apiKey: "QZ+zKps0EK9aXHd15OwnmfFORGOKXD8o8JMWpaHNhZICFkHKuiD3b+wt",
+      secret: "A+kzzhEsmtdu6Aukq8LcWwRCdLTe3sTwRU7uEm5cd3aSF8IiQXB2MdGgdTZlvh/YPAIiNUXnC5zaq1Te6RwDDg=="
     }
   },
 
   addresses: [
     '0x86fE8d6D4C8A007353617587988552B6921514Cb',
-    '0x3941f32a8534a4f608e1fd60c490e30041bbe50c'
+    '0x3941f32a8534a4f608e1fd60c490e30041bbe50c',
+    '0x9dcc35ae915926f7f5e8c624254d91f755d55b71'
   ],
 
   othertokens: {
