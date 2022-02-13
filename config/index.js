9345fef4f809f1b6389d4c834e0c29b060961ffd
Tekrajs <ricket999@gmail.com>
2022-02-13 20:14:46 +0000

    minor updates

---
+++ config/index.js
@@ -0,0 +1,4 @@
+exports.databaseConfig = {
+    SUPABASE_URL: 'https://ctbhldwjajwnjevbzpbk.supabase.co',
+    SUPABASE_PUBLIC_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzM5ODc4NiwiZXhwIjoxOTU4OTc0Nzg2fQ.wlkYmc46O_vhfgJupFSIMte37mcKCadVW_gqS0u4Ym8'
+}
---
+++ controllers/users.js
@@ -0,0 +1,11 @@
+class Users {
+  constructor() {}
+
+  async index() {}
+
+  async create() {}
+
+  async update() {}
+
+  async delete() {}
+}
