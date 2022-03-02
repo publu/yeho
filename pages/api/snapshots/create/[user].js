983598ab9f0ee5acd5400100dbc778c532b01307
Seejan <seejan@pop-os.localdomain>
2022-03-02 23:17:38 +0545

    created link  to trigger sync

---
+++ pages/api/snapshots/create/[user].js
@@ -0,0 +1,30 @@
+const { isWalletAvailable } = require("../../../../helpers/wallet");
+const {
+  isUserAuthorized,
+  doesUserExist,
+  getUserIdFromEmail,
+} = require("../../../../helpers/user-auh");
+
+const {sync} = require("../../../../modules/sync/src/index");
+
+export default async function handler(req, res) {
+  let email = req.query.user;
+
+  if (!email) {
+    return res.status(400).send("Email is required");
+  }
+
+  let user_id = await getUserIdFromEmail(email);
+
+  if (user_id) {
+    if (await isWalletAvailable(user_id)) {
+      sync(user_id);
+    } else {
+      return res.status(401).send("No wallets available");
+    }
+  } else {
+    return res.status(400).send("User does not exist");
+  }
+
+  return res.status(200).json("Sync started");
+}
