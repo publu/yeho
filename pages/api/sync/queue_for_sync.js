23956b783eb6ccbf9a5ba9eb07680a43c317fdd7
Seejan <seejan@pop-os.localdomain>
2022-04-16 17:27:16 +0545

    sync added

--- pages/api/sync/queue_for_sync.js
+++ pages/api/sync/queue_for_sync.js
@@ -1,24 +1,24 @@
-// const {
-// insertEligibleUsersToSyncQueue
-// } = require('../../../modules/sync/src/helpers/sync-helpers');
+const {
+insertEligibleUsersToSyncQueue
+} = require('../../../modules/sync/src/helpers/sync-helpers');
 
 export default async function handler(req, res) {
-  res.status(301).json({ statusCode: 301 });
-  // try {
-  //   let headers = req.headers;
-  //   let tokenHeader = headers['token'];
-  //   let secretHeader = headers['secret'];
-  //   let token = process.env.CRONJOB_TOKEN;
-  //   let secret = process.env.CRONJOB_SECRET;
+  // res.status(301).json({ statusCode: 301 });
+  try {
+    let headers = req.headers;
+    let tokenHeader = headers['token'];
+    let secretHeader = headers['secret'];
+    let token = process.env.CRONJOB_TOKEN;
+    let secret = process.env.CRONJOB_SECRET;
 
-  //   if (!tokenHeader || !secretHeader || tokenHeader !== token || secretHeader !== secret) {
-  //     return res.status(401).json({ success: false, ststusCode: 401, message: 'Unauthorized' });
-  //   }
+    if (!tokenHeader || !secretHeader || tokenHeader !== token || secretHeader !== secret) {
+      return res.status(401).json({ success: false, ststusCode: 401, message: 'Unauthorized' });
+    }
 
-  //   await insertEligibleUsersToSyncQueue();
-  // } catch (error) {
-  //   return res.status(500).json({ success: false, statusCode: 500, message: error });
-  // }
+    await insertEligibleUsersToSyncQueue();
+  } catch (error) {
+    return res.status(500).json({ success: false, statusCode: 500, message: error });
+  }
 
-  // res.status(200).json({ success: true, statusCode: 200, message: 'OK' });
+  res.status(200).json({ success: true, statusCode: 200, message: 'OK' });
 }
--- pages/api/sync/sync.js
+++ pages/api/sync/sync.js
@@ -1,42 +1,42 @@
-// const { sync } = require('../../../modules/sync/src/index');
+const { sync } = require('../../../modules/sync/src/index');
 
-// const {
-//   getElementFromSyncQueue,
-//   removeElementFromSyncQueue,
-//   markQueueElementAsProcessing,
-// } = require('../../../modules/sync/src/helpers/sync-helpers');
+const {
+  getElementFromSyncQueue,
+  removeElementFromSyncQueue,
+  markQueueElementAsProcessing,
+} = require('../../../modules/sync/src/helpers/sync-helpers');
 
 export default async function handler(req, res) {
-  res.status(301).json({ statusCode: 301 });
+  // res.status(301).json({ statusCode: 301 });
 
-  // try {
-  //   let headers = req.headers;
-  //   let tokenHeader = headers['token'];
-  //   let secretHeader = headers['secret'];
-  //   let token = process.env.CRONJOB_TOKEN;
-  //   let secret = process.env.CRONJOB_SECRET;
+  try {
+    let headers = req.headers;
+    let tokenHeader = headers['token'];
+    let secretHeader = headers['secret'];
+    let token = process.env.CRONJOB_TOKEN;
+    let secret = process.env.CRONJOB_SECRET;
 
-  //   let user_id = false;
+    let user_id = false;
 
-  //   if (!tokenHeader || !secretHeader || tokenHeader !== token || secretHeader !== secret) {
-  //     return res.status(401).json({ success: false, ststusCode: 401, message: 'Unauthorized' });
-  //   }
+    if (!tokenHeader || !secretHeader || tokenHeader !== token || secretHeader !== secret) {
+      return res.status(401).json({ success: false, ststusCode: 401, message: 'Unauthorized' });
+    }
 
-  //   let queueItem = await getElementFromSyncQueue();
-  //   if (queueItem) {
-  //     user_id = queueItem[0].user_id;
-  //     console.log('Running sync from queue');
+    let queueItem = await getElementFromSyncQueue();
+    if (queueItem) {
+      user_id = queueItem[0].user_id;
+      console.log('Running sync from queue');
 
-  //     markQueueElementAsProcessing(user_id);
-  //     sync(user_id);
-  //     removeElementFromSyncQueue(user_id);
-  //   } else {
-  //     console.log('No queue item found');
-  //   }
-  // } catch (error) {
-  //   removeElementFromSyncQueue(user_id);
-  //   return res.status(500).json({ success: false, statusCode: 500, message: error });
-  // }
+      markQueueElementAsProcessing(user_id);
+      sync(user_id);
+      removeElementFromSyncQueue(user_id);
+    } else {
+      console.log('No queue item found');
+    }
+  } catch (error) {
+    removeElementFromSyncQueue(user_id);
+    return res.status(500).json({ success: false, statusCode: 500, message: error });
+  }
 
-  // res.status(200).json({ success: true, statusCode: 200, message: 'OK' });
+  res.status(200).json({ success: true, statusCode: 200, message: 'OK' });
 }
